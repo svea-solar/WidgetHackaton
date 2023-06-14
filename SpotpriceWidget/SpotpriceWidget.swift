6596b677cfe18fa5d70daacb72f8887781c620a7
jacobfredriksson <jacobfredriksson@live.se>
2023-06-14 10:20:31 +0200

    spotprice widget

---
+++ SpotpriceWidget/SpotpriceWidget.swift
@@ -0,0 +1,71 @@
+//
+//  SpotpriceWidget.swift
+//  SpotpriceWidget
+//
+//  Created by Jacob Fredriksson on 2023-06-14.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct SpotpriceWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        //Doing ui here
+        VStack {
+            Text("Spotprices")
+        }
+    }
+}
+
+struct SpotpriceWidget: Widget {
+    let kind: String = "SpotpriceWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            SpotpriceWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct SpotpriceWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotpriceWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ SpotpriceWidget/SpotpriceWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SpotpriceWidgetBundle.swift
+//  SpotpriceWidget
+//
+//  Created by Jacob Fredriksson on 2023-06-14.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SpotpriceWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SpotpriceWidget()
+        SpotpriceWidgetLiveActivity()
+    }
+}
