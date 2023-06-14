c73fbbf299a9826ecbfefc3f29a977f285ac5210
jacobfredriksson <jacobfredriksson@live.se>
2023-06-14 10:09:07 +0200

    Initial Commit

---
+++ WidgetHackaton/WidgetHackatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHackatonApp.swift
+//  WidgetHackaton
+//
+//  Created by Jacob Fredriksson on 2023-06-14.
+//
+
+import SwiftUI
+
+@main
+struct WidgetHackatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
